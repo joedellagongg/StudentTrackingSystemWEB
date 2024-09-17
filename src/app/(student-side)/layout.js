80855c1bdc509dace76717c0ae50e58a014a9b8f
Kenneth1219 <kmanuel531@gmail.com>
2024-09-17 21:06:26 +0800

    fix: logout button

--- src/app/(student-side)/layout.js
+++ src/app/(student-side)/layout.js
@@ -18,7 +18,7 @@ export default function DashboardLayout({ children }) {
   return (
     <main className=" bg-[#E1E8FF] h-screen w-screen p-4 flex flex-row">
       {/* side bar */}
-      <div className=" relative w-[20%] h-full bg-[#FFFFFF] rounded-2xl">
+      <div className=" relative w-[20%] h-full bg-[#FFFFFF] rounded-2xl overflow-y-scroll no-scrollbar">
         <div className=" p-4 w-full flex justify-center items-center border-b-2">
           <img src="./logo/logo.svg" className=" h-20" />
         </div>
@@ -58,12 +58,10 @@ export default function DashboardLayout({ children }) {
             <p className=" text-[#002147] text-[16px]">Digital ID</p>
           </button>
 
-          <div className=" w-full pr-8 absolute bottom-4">
-            <button className=" hover:bg-[#E1E8FF] w-full flex flex-row items-center gap-x-4 rounded-xl p-2">
-              <img src="./icons/logout-icon.svg" />
-              Log Out
-            </button>
-          </div>
+          <button className=" hover:bg-[#E1E8FF] w-full flex flex-row items-center gap-x-4 rounded-xl p-2">
+            <img src="./icons/logout-icon.svg" />
+            Log Out
+          </button>
         </div>
       </div>
 
@@ -75,7 +73,7 @@ export default function DashboardLayout({ children }) {
         <div className=" p-4 h-[30%] w-full bg-[#FFFFFF] rounded-2xl flex flex-col justify-center items-center gap-y-2">
           <DateTime />
         </div>
-        <div className="w-full h-full bg-[#FFFFFF] rounded-2xl p-4">
+        <div className="w-full h-full bg-[#FFFFFF] rounded-2xl p-4 overflow-y-scroll no-scrollbar">
           <p className=" text-[#002147] text-[22px] p-2">Schedule</p>
           <div className=" w-full flex flex-col justify-center gap-y-2 no-scrollbar overflow-y-scroll">
             <Schedule />
