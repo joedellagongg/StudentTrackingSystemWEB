26d7203a74442b5dff7865774ad7cb6b09883dee
Kenneth1219 <kmanuel531@gmail.com>
2024-10-06 23:21:53 +0800

    admin, sections

--- client/src/app/(admin-side)/admin/page.js
+++ client/src/app/(admin-side)/admin/page.js
@@ -1,9 +1,9 @@
 import Section from "@/components/sections";
-export default function Student_info() {
+export default function Admin() {
   return (
     <main className=" pl-4 pt-4  w-full h-full">
       <div className=" w-full h-full rounded-2xl flex flex-col bg-[#ffffff]">
-        student info
+        <Section />
       </div>
     </main>
   );
