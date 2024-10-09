51d25f3aa57b95399fc4fb68df0c08bef53b9dd7
Kenneth1219 <kmanuel531@gmail.com>
2024-10-09 23:22:45 +0800

    student_profile, navigations

--- client/src/app/(devs)/add_student/page.tsx
+++ client/src/app/(devs)/add_student/page.tsx
@@ -61,7 +61,7 @@ export default function AddStudent({ closeModal }) {
   return (
     <div className="z-50 bg-black bg-opacity-50 w-full h-full absolute top-0 left-0 flex items-center justify-center">
       {/* <div className="h-96 w-10 bg-red-500"></div> */}
-      <div className="bg-white flex justify-center items-center p-4 w-[80%] h-[80%] rounded-xl overflow-y-scroll">
+      <div className="bg-white flex justify-center items-center p-4 h-[80%] rounded-xl overflow-y-scroll">
         <form
           onSubmit={handleSubmit}
           className=" flex flex-col justify-center items-center"
---
+++ client/src/components/student_info.js
@@ -0,0 +1,28 @@
+const student_info = [
+    {
+        id: 1,
+        stud_id: "123-456",
+        nfc_id: "123-456-789",
+        pic: "./images/profile.svg",
+        fname: "Kenneth",
+        mname: "C.",
+        lname: "Manuel",
+        grade: "11",
+        strand: "HUMSS",
+        section: "Mactan",
+        age: "18",
+        birthday: "12/19/2002",
+        gender: "Male",
+        address: "Poblacion, Pandi, Bulacan",
+        emailAdd: "example@gmail.com",
+        contactNum: "09123456789",
+        fathersname: "Father C. Manuel",
+        fathersNo: "09987654321",
+        mothersname: "Mother C. Manuel",
+        mothersNo: "09123459876",
+        guardian: "Guardian C. Manuel",
+        guardianNo: "09987651234",
+    }
+];
+
+export default student_info;
