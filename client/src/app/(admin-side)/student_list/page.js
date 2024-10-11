5a71630bcb615a0c99496edbc2d2bdfc8b4f285d
Kenneth1219 <kmanuel531@gmail.com>
2024-10-12 03:21:21 +0800

    update

--- client/src/app/(admin-side)/student_list/page.js
+++ client/src/app/(admin-side)/student_list/page.js
@@ -7,119 +7,110 @@ import { useEffect } from "react";
 import AddStudent from "@/app/(devs)/add_student/page";
 
 export default function Stud_list() {
-    const router = useRouter();
-    const navigate = (path) => {
-        router.push(path);
+  const router = useRouter();
+  const navigate = (path) => {
+    router.push(path);
+  };
+
+  const section = [
+    {
+      id: 1,
+      strand: "HUMSS",
+      grade: "11",
+      section: "Mactan",
+    },
+  ];
+
+  const openModal = () => setModal(true);
+  const [modal, setModal] = useState(false);
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        const response = await axios.get("http://localhost:5500/student_list");
+        setStudents(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError("Failed to fetch students. Check Servers");
+        console.error(err);
+        setLoading(false);
+      }
     };
 
-    const section = [
-        {
-            id: 1,
-            strand: "HUMSS",
-            grade: "11",
-            section: "Mactan",
-        },
-    ];
+    fetchStudents();
+  }, []);
 
-    const openModal = () => setModal(true);
-    const [modal, setModal] = useState(false);
-    const [students, setStudents] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-    useEffect(() => {
-        const fetchStudents = async () => {
-            try {
-                const response = await axios.get(
-                    "http://localhost:5500/student_list",
-                );
-                setStudents(response.data);
-                setLoading(false);
-            } catch (err) {
-                setError("Failed to fetch students. Check Servers");
-                console.error(err);
-                setLoading(false);
-            }
-        };
+  if (error) {
+    return <p>{error}</p>;
+  }
 
-        fetchStudents();
-    }, []);
+  return (
+    <main className=" w-full h-full p-4 rounded-2xl bg-[#ffffff] overflow-y-scroll no-scrollbar">
+      <div className=" w-full h-full flex flex-col">
+        <div className=" flex flex-row border-b pb-4 ">
+          <button onClick={() => navigate("../dashboard", "admin")}>
+            <img src="./icons/back-icon.svg" alt="" className=" h-[50px]" />
+          </button>
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
-
-    if (error) {
-        return <p>{error}</p>;
-    }
-
-    return (
-        <main className=" w-full h-full p-4 rounded-2xl bg-[#ffffff] overflow-y-scroll no-scrollbar">
-            <div className=" w-full h-full flex flex-col">
-                <div className=" flex flex-row border-b pb-4 ">
-                    <button onClick={() => navigate("../dashboard", "admin")}>
-                        <img
-                            src="./icons/back-icon.svg"
-                            alt=""
-                            className=" h-[50px]"
-                        />
-                    </button>
-
-                    {section.map((items) => (
-                        <div
-                            key={items.id}
-                            className=" w-full flex items-center justify-center no-scrollbar "
-                        >
-                            <h1 className=" text-3xl">
-                                {items.strand} {items.grade} - {items.section}
-                            </h1>
-                        </div>
-                    ))}
-                </div>
-                <div className="w-full h-[90%] overflow-y-scroll flex justify-center no-scrollbar">
-                    <table className="w-[80%] bg-white border-collapse no-scrollbar">
-                        <tbody>
-                            {students.map((list) => (
-                                <tr key={list.id} className="border-b">
-                                    <td className="p-4">
-                                        <img
-                                            src={`http://localhost:5500/${students.image_profile}/`}
-                                            alt="Student Picture"
-                                            className="h-[60px] rounded-full"
-                                        />
-                                    </td>
-                                    <td className="p-4">{list.student_id}</td>
-                                    <td className="p-4 text-center">
-                                        {list.lname}, {list.fname} {list.mname}
-                                    </td>
-                                    <td className="p-4 text-end">
-                                        <button
-                                            onClick={() =>
-                                                navigate(
-                                                    "../student_profile",
-                                                    "student_profile",
-                                                )
-                                            }
-                                            className="bg-[#002147] text-white p-3 text-sm rounded-lg"
-                                        >
-                                            View Profile
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-
-                <button
-                    onClick={openModal}
-                    className=" absolute self-end bottom-8 right-8"
-                >
-                    <img src="./icons/add-icon.svg" className=" h-[80px]" />
-                </button>
-
-                {modal && <AddStudent closeModal={() => setModal(false)} />}
+          {section.map((items) => (
+            <div
+              key={items.id}
+              className=" w-full flex items-center justify-center no-scrollbar "
+            >
+              <h1 className=" text-3xl">
+                {items.strand} {items.grade} - {items.section}
+              </h1>
             </div>
-        </main>
-    );
+          ))}
+        </div>
+        <div className="w-full h-[90%] overflow-y-scroll flex justify-center no-scrollbar">
+          <table className="w-[80%] max-h-[20%] bg-white border-collapse no-scrollbar">
+            <tbody>
+              {students.map((list) => (
+                <tr key={list.id} className="border-b">
+                  <td className="p-4">
+                    <img
+                      src={`http://localhost:5500/${students.image_profile}/`}
+                      alt="Student Picture"
+                      className="h-[60px] rounded-full"
+                    />
+                  </td>
+                  <td className="p-4">{list.student_id}</td>
+                  <td className="p-4 text-center">
+                    {list.lname}, {list.fname} {list.mname}
+                  </td>
+                  <td className="p-4 text-end">
+                    <button
+                      onClick={() =>
+                        navigate("../student_profile", "student_profile")
+                      }
+                      className="bg-[#002147] text-white p-3 text-sm rounded-lg"
+                    >
+                      View Profile
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <button
+          onClick={openModal}
+          className=" absolute self-end bottom-8 right-8"
+        >
+          <img src="./icons/add-icon.svg" className=" h-[80px]" />
+        </button>
+
+        {modal && <AddStudent closeModal={() => setModal(false)} />}
+      </div>
+    </main>
+  );
 }
