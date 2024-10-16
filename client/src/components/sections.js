bba7d799db589ae2f1121d97b2bb6c4d6da3df36
Kenneth1219 <kmanuel531@gmail.com>
2024-10-16 22:42:28 +0800

    fix, add loader

--- client/src/components/sections.js
+++ client/src/components/sections.js
@@ -4,62 +4,61 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
+import Loader from "@/components/loader";
 
 export default function Section() {
-    const router = useRouter();
-    const navigate = (path) => {
-        router.push(path);
+  const router = useRouter();
+  const navigate = (path) => {
+    router.push(path);
+  };
+
+  const [sections, setSections] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        const response = await axios.get(
+          "https://attendance-backend-app.up.railway.app/get_section"
+        );
+        setSections(response.data);
+        console.log(response.data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to fetch section. Check Servers");
+        console.error(err);
+        setLoading(false);
+      }
     };
 
-    const [sections, setSections] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    fetchStudents();
+  }, []);
 
-    useEffect(() => {
-        const fetchStudents = async () => {
-            try {
-                const response = await axios.get(
-                    "https://attendance-backend-app.up.railway.app/get_section",
-                );
-                setSections(response.data);
-                console.log(response.data);
-                setLoading(false);
-            } catch (err) {
-                console.log(err);
-                setError("Failed to fetch section. Check Servers");
-                console.error(err);
-                setLoading(false);
-            }
-        };
+  if (loading) {
+    return <Loader />;
+  }
 
-        fetchStudents();
-    }, []);
+  if (error) {
+    return <p>{error}</p>;
+  }
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
-
-    if (error) {
-        return <p>{error}</p>;
-    }
-
-    return (
-        <main className="grid grid-cols-3 p-6 gap-3 h-full">
-            {sections.map((item) => (
-                <button
-                    key={item.id}
-                    onClick={() =>
-                        navigate(`../student_list?section=${item.id}`)
-                    }
-                    className="bg-[#002147] rounded-xl text-white text-center text-xl h-[150px] 
+  return (
+    <main className="grid grid-cols-3 p-6 gap-3 h-full">
+      {sections.map((item) => (
+        <button
+          key={item.id}
+          onClick={() => navigate(`../student_list?section=${item.id}`)}
+          className="bg-[#002147] rounded-xl text-white text-center text-xl h-[150px] 
           hover:bg-white drop-shadow-2xl border hover:text-black"
-                >
-                    <p>
-                        {item.strand} - {item.grade_level}
-                    </p>
-                    <p>{item.section_name}</p>
-                </button>
-            ))}
-        </main>
-    );
+        >
+          <p className=" uppercase">
+            {item.strand} - {item.grade_level}
+          </p>
+          <p className=" capitalize">{item.section_name}</p>
+        </button>
+      ))}
+    </main>
+  );
 }
