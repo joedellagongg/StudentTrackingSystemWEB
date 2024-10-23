6f651aa79aab7a8528d468a4ef5a15bbab93bd38
Joedel Lagong <joedelsalaslagong@gmail.com>
2024-10-23 12:50:35 +0800

    Delete server directory

--- server/routes/section.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const studentController = require("../controllers/student.controllers");
-
-router.get("/", studentController.getStudent);
-// router.post("/", studentController.addStudent);
-
-module.exports = router;
