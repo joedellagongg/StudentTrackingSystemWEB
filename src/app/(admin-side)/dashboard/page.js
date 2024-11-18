d131c18eb28a6a9109eda00da316e22df0870087
Joedel Lagong <joedelsalaslagong@gmail.com>
2024-11-18 21:51:24 +0800

    push

--- src/app/(admin-side)/dashboard/page.js
+++ src/app/(admin-side)/dashboard/page.js
@@ -1,9 +1,9 @@
-import DashboardSections from '@/components/dashboardSections';
+import DashboardSections from "@/components/dashboardSections";
 
 export default function Admin() {
-  return (
-    <main className="w-full h-full rounded-2xl overflow-x-scroll bg-[#ffffff]">
-      <DashboardSections />
-    </main>
-  );
+    return (
+        <main className="w-full h-full rounded-2xl overflow-x-scroll bg-[#ffffff]">
+            <DashboardSections />
+        </main>
+    );
 }
--- src/components/dashboardSections.jsx
+++ src/components/dashboardSections.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from "react";
+
 import axios from "axios";
 import Section from "@/components/sections";
 import Loader from "@/components/loader";
