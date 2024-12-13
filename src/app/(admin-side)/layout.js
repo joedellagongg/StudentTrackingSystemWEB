9c09c5d4fa94d0202e8209d2e5b5e446dc8e985c
Kenneth1219 <kmanuel531@gmail.com>
2024-12-14 01:17:13 +0800

    download csv, add button modification

--- src/app/(admin-side)/layout.js
+++ src/app/(admin-side)/layout.js
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import DateTime from "../../components/time&date";
 import SideNav from "../../components/side_nav";
 
 export default function DashboardLayout({ children }) {
@@ -10,14 +9,6 @@ export default function DashboardLayout({ children }) {
       <SideNav />
 
       <div className=" flex flex-col w-full">
-        {/* datetime */}
-        {/* <div className=" pl-4 w-full h-[15%] flex flex-row items-center justify-between gap-y-4 mb-4">
-          <h1 className=" text-[45px]">Hello, Admin</h1>
-          <div className=" p-4 w-[25%] h-full bg-[#FFFFFF] rounded-2xl flex flex-col justify-center items-center gap-y-2">
-            <DateTime />
-          </div>
-        </div> */}
-
         {/* landing page */}
         {children}
       </div>
