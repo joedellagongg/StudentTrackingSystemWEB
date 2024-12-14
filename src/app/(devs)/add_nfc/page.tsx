5b8f0696316bb3da85408ce3f01ec86331cf5703
Joedel Salas Lagong <joedelsalaslagong@gmail.com>
2024-12-15 07:06:52 +0800

    fix: add NFC Recorder component.

---
+++ src/app/(devs)/add_nfc/page.tsx
@@ -0,0 +1,12 @@
+export default function nfcRecorder() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-gray-300 ">
+      {/* <h1>Hello Pretty Little Baby</h1> */}
+
+      <div className="h-[50%] w-[50%] bg-blue-400 flex justify-center ">
+        <h5 className="p-5">Student NFC Recorder</h5>
+        
+      </div>
+    </div>
+  );
+}
