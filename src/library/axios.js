064330d97f48e70721e595f2b0fbcb933de6b83e
Joedel Lagong <joedelsalaslagong@gmail.com>
2025-01-25 22:16:27 +0800

    fix: change to online endpoint

--- src/library/axios.js
+++ src/library/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://attendance-backend-app.up.railway.app",
-  baseURL: "http://localhost:5500",
+  baseURL: "https://attendance-backend-app.up.railway.app",
+  // baseURL: "http://localhost:5500",
   // Change mo yung IP ADDRESS MO DITO hanggang 192.168.1.104 | port nung server
 
   // timeout: 5000, // Optional timeout
